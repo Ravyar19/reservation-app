5e31c6fc49e0a40d1ab4edb5ac0aa8552919005c
Ravyar19 <ravyar.ara@kruschecompany.com>
2023-06-01 04:59:32 +0300

    move json sol to env

--- pages/api/getReceiptNumber.js
+++ pages/api/getReceiptNumber.js
@@ -1,9 +1,14 @@
-let receiptNumber = 0;
+import fs from "fs";
+import path from "path";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
-      res.status(200).json({ receiptNumber: receiptNumber });
+      const filePath = path.join(process.cwd(), "receiptNumber.json");
+      const fileData = fs.readFileSync(filePath, "utf8");
+      const receiptData = JSON.parse(fileData);
+
+      res.status(200).json({ receiptNumber: receiptData.current });
     } catch (error) {
       console.error(error);
       res
@@ -13,8 +18,4 @@ export default async function handler(req, res) {
   } else {
     res.status(405).json({ error: "Method not allowed." });
   }
-}
-
-export function incrementReceiptNumber() {
-  receiptNumber++;
 }
